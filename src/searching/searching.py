3fa55cac6d00e36fe0b36a3234e19494ac495106
jaren <jarensmart@yahoo.com>
2020-09-03 21:36:59 -0400

    Finished iterative sorting

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,19 @@
 def linear_search(arr, target):
     # Your code here
 
-
     return -1   # not found
-
+    # Loop through the array
+    for i in arr:
+        # compare values
+        if i == target:
+            return True
+    return False
 
 # Write an iterative implementation of Binary Search
+
+
 def binary_search(arr, target):
 
     # Your code here
-
 
     return -1  # not found
